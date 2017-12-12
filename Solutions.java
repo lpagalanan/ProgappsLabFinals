f4eed08ff135e6d96ad02152e69fa351bfd8e5f5
Noriel Jon P. Navarro <norieljon.navarro.1998@gmail.com>
2017-12-12 21:37:48 +0800

    Navarro Noriel Jon, Added his codes

--- Solutions.java
+++ Solutions.java
@@ -45,6 +45,83 @@ public class Solutions{
 	    return count == 1? "YES":"NO";
 	}
 
+	public static int QuasiPalindrome (int number, int temp, int holder, int numberReverse){
+        number = 0;
+        temp = 0;
+        holder = 0;
+        numberReverse = 0;
 
+        Scanner kbd = new Scanner(System.in);
+        number = kbd.nextInt();
+
+        temp = number;
+        while(number > 0) {
+            holder = number % 10;
+            numberReverse = (numberReverse *10) + holder;
+            number /= 10;
+        }
+        
+        while(temp % 10 == 0){
+        	temp /= 10;
+        }
+        
+        if(numberReverse == temp){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+
+    public int Fraction(int number, int numerator, int denominator) {
+
+    public static void main(String[] args){
+    	Scanner kbd = new Scanner(System.in);
+    	
+    	number = Integer.parseInt(kbd.nextLine());
+    	numerator = (number-1) / 2;
+    	denominator = (number+1) / 2;
+    	
+    	if(numerator + denominator != number){
+    		denominator++;
+    	}
+    	
+    	for(int i = 1; i < 9; i++){
+    		if(numerator % i == 0 && denominator % i == 0){
+    			numerator /= i;
+    			denominator /= i;
+    		}
+    		if(numerator % 2 == 0 && denominator % 2 == 0){
+    			numerator--;
+    			denominator++;
+    		}
+    	} 	
+    	System.out.println(numerator);
+    	System.out.println(denominator);
+    }
+
+    public int FairGame(int array[], int com[], int sto, int n, int x) {
+        Scanner kbd = new Scanner(System.in);
+        array[] = new int[101];
+        com[] = new int[100];
+        sto = 0;
+        n = kbd.nextInt();
+        x;
+
+        for (int i = 0; i < n; i++) {
+            x = kbd.nextInt();
+            if(++array[x]==1){
+                com[sto++]=x;
+            }
+        }
+        if (sto == 2 && array[com[0]] == array[com[1]]) {
+            System.out.println("YES");
+            System.out.println(com[0]+" "+com[1]);
+
+
+        } else {
+            System.out.println("NO");
+
+        }
+}
 
 }
